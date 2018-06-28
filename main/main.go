a7eb79de8c591da55ba0db9833a205009d449331
MalesSebastian <malessebastian@gmail.com>
2018-06-28 16:02:21 +0300

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to Godow! A rudimentary download manager written in golang!")
+	PrintCommands()
+	reader := bufio.NewReader(os.Stdin)
+	input := ""
+	for input != "exit" {
+		fmt.Print("> ")
+		input, _ = reader.ReadString('\n')
+		input = strings.TrimRight(input, "\n")
+		switch input {
+
+		}
+	}
+}
---
+++ main/utils.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrintCommands() {
+	fmt.Println("1: Download file \n" +
+		"2: See current downloads \n" +
+		"3: Set default path \n" +
+		"exit: exit the program")
+}
